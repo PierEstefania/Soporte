63fe650c67f911e5efa02331eeb330cb5c47b3b3
unknown <pierestefania13@gmail.com>
2021-02-22 21:53:56 -0300

    sisisis

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <Header/> 
+
+    <v-main>
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+ import Header from './components/Header/Header';
+
+export default {
+  name: 'App',
+
+  components: {
+     Header,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,18 @@
+<template>
+   <v-app-bar
+      app
+      color="white"
+      dark
+    >
+    <img>
+    
+    </v-app-bar>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({})
+  }
+  </script>
